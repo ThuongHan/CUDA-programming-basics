f41542678ed4652daa02e1777a0d6c5bb7bf1d75
Mateusz Susmarski <scur0149@int4.local.snellius.surf.nl>
2026-05-09 20:59:40 +0200

    device properties

---
+++ get_cuda_properties/temp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_a100
+#SBATCH --time=00:10:00
+
+module load 2025
+module load CUDA/12.9.1
+
+srun enum
